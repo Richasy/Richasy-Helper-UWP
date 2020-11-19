1dda3bb4b15692438f045fa99e26f80b980b63dd
Richasy <Thansy@foxmail.com>
2020-11-19 15:07:43 +0800

    添加爱词霸工具

---
+++ Richasy-Helper-UWP/Models/iCiBaModels.cs
@@ -0,0 +1,63 @@
+﻿namespace Richasy.Helper.UWP.Models
+{
+    public class Translate
+    {
+        public int status { get; set; }
+        public Content content { get; set; }
+    }
+
+    public class Content
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string @out { get; set; }
+        public string vendor { get; set; }
+        public int err_no { get; set; }
+    }
+
+    public class Ciba
+    {
+        public string word_name { get; set; }
+        public string is_CRI { get; set; }
+        public Exchange exchange { get; set; }
+        public Symbol[] symbols { get; set; }
+    }
+
+    public class Exchange
+    {
+        public string[] word_pl { get; set; }
+        public string[] word_third { get; set; }
+        public string[] word_past { get; set; }
+        public string[] word_done { get; set; }
+        public string[] word_ing { get; set; }
+        public string[] word_er { get; set; }
+        public string[] word_est { get; set; }
+    }
+
+    public class Symbol
+    {
+        public string ph_en { get; set; }
+        public string ph_am { get; set; }
+        public string ph_other { get; set; }
+        public string ph_en_mp3 { get; set; }
+        public string ph_am_mp3 { get; set; }
+        public string ph_tts_mp3 { get; set; }
+        public string word_symbol { get; set; }
+        public string symbol_mp3 { get; set; }
+        public Part[] parts { get; set; }
+    }
+
+    public class Part
+    {
+        public string part { get; set; }
+        public string[] means { get; set; }
+        public Mean[] means_other { get; set; }
+    }
+
+    public class Mean
+    {
+        public string word_mean { get; set; }
+        public string has_mean { get; set; }
+        public int split { get; set; }
+    }
+}
---
+++ Richasy-Helper-UWP/iCiBaHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Richasy.Helper.UWP.Models;
+
+namespace Richasy.Helper.UWP
+{
+    public class iCiBaHelper
+    {
+        private Instance instance;
+        private const string iCiBaKey = "FD6631A6A5BC97C124D85CDCAFCA7806";
+        public iCiBaHelper(Instance ins)
+        {
+            instance = ins;
+        }
+        /// <summary>
+        /// 从网络获取单词释义
+        /// </summary>
+        /// <param name="text">单词</param>
+        /// <param name="targetLan">目标语言</param>
+        /// <returns></returns>
+        public async Task<Ciba> GetWebICiBaModel(string text,string targetLan)
+        {
+            string url = $"http://dict-co.iciba.com/api/dictionary.php?w={text.ToLower()}&type=json&key={iCiBaKey}&t={targetLan}&f=auto";
+            Ciba data = null;
+            try
+            {
+                var response = await instance.Net.GetTextFromWebAsync(url);
+                if (response.Contains("\"word_mean\":"))
+                {
+                    response = response.Replace("\"means\":", "\"means_other\":");
+                }
+                data = JsonConvert.DeserializeObject<Ciba>(response);
+            }
+            catch (Exception) { }
+            return data;
+        }
+        /// <summary>
+        /// 获取词霸的翻译
+        /// </summary>
+        /// <param name="text">内容</param>
+        /// <param name="targetLan">目标语言</param>
+        /// <returns></returns>
+        public async Task<Translate> GetICiBaTranslate(string text,string targetLan)
+        {
+            string url = $"http://fy.iciba.com/ajax.php?a=fy";
+            var client = new HttpClient();
+            var kvs = new Dictionary<string, string>();
+            kvs.Add("f", "auto");
+            kvs.Add("t", targetLan);
+            kvs.Add("w", text);
+            var pos = new FormUrlEncodedContent(kvs);
+            Translate result = null;
+            using (client)
+            {
+                try
+                {
+                    var data = await client.PostAsync(url, pos);
+                    if (data.IsSuccessStatusCode)
+                    {
+                        string content = await data.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<Translate>(content);
+                    }
+                }
+                catch (Exception) { }
+            }
+            return result;
+        }
+    }
+}
